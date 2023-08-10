8ef61006c70e4135e1ffeb633a0f063c7ac91d8e
Gabriel Mendes da Silva <122908813+GabrielMendes06@users.noreply.github.com>
2023-08-10 11:29:38 -0300

    new

--- src/routes.js
+++ src/routes.js
@@ -7,4 +7,4 @@ router.delete('/users/:id', LoginController.handleDelete);
 router.post('/new-user', LoginController.handlePost);
 router.put('/users/:id', LoginController.handleUpdate);
 
-module.exports = router;
+module.exports = router; 
